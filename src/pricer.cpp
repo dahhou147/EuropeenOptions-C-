1f104f7f0f8284ef6ed226ee2ff9b58b4392b217
dahhou147 <odahou89@gmail.com>
2025-07-19 18:48:28 +0200

    Heston

--- src/pricer.cpp
+++ src/pricer.cpp
@@ -1,185 +1,111 @@
 #include <algorithm>
 #include <cmath>
+#include <complex>
 #include <fstream>
 #include <iostream>
 #include <numeric>
 #include <random>
+#include <utility>
 #include <vector>
-
 using namespace std;
 
-vector<vector<double>> GBM(double S0, double T, double sigma, double drift, int N, int M) {
-    double dt = T / N;
+double norm_cdf(double x) {
+    return 0.5 * erfc(-x * sqrt(2) / 2);
+}
+
+double norm_pdf(double x) {
+    return exp(-x * x / 2) / sqrt(2 * M_PI);
+}
+
+vector<vector<double>> HestonSimultion(double S0, double drift, double T, double v0, double kappa,
+                                       double theta, double sigma, double rho, int N, int M) {
+    double dt = T / M;
     double sqrt_dt = sqrt(dt);
+    vector<vector<double>> stock_path(M, vector<double>(N + 1));
+    vector<vector<double>> variance_path(M, vector<double>(N + 1));
     random_device rd;
     mt19937 gen(rd());
-    vector<vector<double>> stock_path(M, vector<double>(N + 1));
     for (int m = 0; m < M; m++) {
         stock_path[m][0] = S0;
+        variance_path[m][0] = v0;
         for (int i = 1; i <= N; i++) {
-            normal_distribution<> dW(0, sqrt_dt);
-            double x = dW(gen);
+            normal_distribution<> dW1(0, sqrt_dt);
+            normal_distribution<> dw2(0, sqrt_dt);
+            double z1 = dW1(gen);
+            double z2 = rho * z1 + sqrt(1 - rho * rho) * dw2(gen);
+            double v = variance_path[m][i - 1] + kappa * (theta - variance_path[m][i - 1]) * dt +
+                       sigma * sqrt(variance_path[m][i - 1]) * z1;
+            variance_path[m][i] = max(0.0, v);
             stock_path[m][i] =
-                stock_path[m][i - 1] * exp((drift - 0.5 * sigma * sigma) * dt + sigma * x);
+                stock_path[m][i - 1] * (1 + drift * dt + sqrt(variance_path[m][i]) * z2);
         }
     }
     return stock_path;
 }
 
-class PricerBS {
-public:
+class HestonPricer {
+private:
     double S0;
+    double drift;
     double T;
+    double v0;
+    double kappa;
+    double theta;
     double sigma;
+    double rho;
     double r;
-    double K;
-    double q;
+    int N;
+    int M;
 
-    PricerBS(double S0, double T, double sigma, double r, double K, std::string type,
-             double deviden)
-        : S0(S0), T(T), sigma(sigma), r(r), K(K), q(deviden) {}
-
-    PricerBS(const PricerBS& other)
-        : S0(other.S0), T(other.T), sigma(other.sigma), r(other.r), K(other.K), q(other.q) {}
-
-    double get_price_call(std ::string type) {
-        double d1 = (log(S0 / K) + (r - q + 0.5 * sigma * sigma) * T) / (sigma * sqrt(T));
-        double d2 = d1 - sigma * sqrt(T);
-        switch (type) {
-            case "call":
-                return S0 * exp(-q * T) * norm_cdf(d1) - K * exp(-r * T) * norm_cdf(d2);
-            case "put":
-                return K * exp(-r * T) * norm_cdf(-d2) - S0 * exp(-q * T) * norm_cdf(-d1);
-            default:
-                throw std::invalid_argument("Invalid option type");
-        }
-    }
-};
-
-class Greeks {
-    PricerBS pricer;
-    double tau;
-    Greeks(PricerBS pricer, double tau) : pricer(pricer), tau(tau) {}
-
-    double delta(std::string type) {
-        double d1 = (log(pricer.S0 / pricer.K) +
-                     (pricer.r - pricer.q + 0.5 * pricer.sigma * pricer.sigma) * tau) /
-                    (pricer.sigma * sqrt(tau));
-        switch (type) {
-            case "call":
-                return exp(-pricer.q * tau) * norm_cdf(d1);
-            case "put":
-                return exp(-pricer.q * tau) * (norm_cdf(d1) - 1);
-            default:
-                throw std::invalid_argument("Invalid option type");
-        }
+public:
+    HestonPricer(double S0, double drift, double T, double v0, double kappa, double theta,
+                 double sigma, double rho, double r, int N, int M) {
+        this->S0 = S0;
+        this->drift = drift;
+        this->T = T;
+        this->v0 = v0;
+        this->kappa = kappa;
+        this->theta = theta;
+        this->sigma = sigma;
+        this->rho = rho;
+        this->r = r;
+        this->N = N;
+        this->M = M;
     }
 
-    double gamma() {
-        double d1 = (log(pricer.S0 / pricer.K) +
-                     (pricer.r - pricer.q + 0.5 * pricer.sigma * pricer.sigma) * tau) /
-                    (pricer.sigma * sqrt(tau));
-        return exp(-pricer.q * tau) * norm_pdf(d1) / (pricer.S0 * pricer.sigma * sqrt(tau));
+    complex<double> carateristique_function(double u, double V, double X) {
+        complex<double> i(0, 1);
+        double M = sqrt(pow(rho * sigma * i * u - kappa, 2) + pow(sigma, 2) * (i * u + pow(u, 2)));
+        complex<double> N = (rho * sigma * i * u - kappa - M) / (rho * sigma * i * u - kappa + M);
+        complex<double> A = r * i * u * T + (kappa * theta / pow(sigma, 2)) *
+                                                ((rho * sigma * i * u - kappa - M) * T -
+                                                 2 * log((1 - N * exp(M * T)) / (1 - N)));
+        complex<double> C = (exp(M * T) - 1) * (rho * sigma * i * u - kappa - M) /
+                            (pow(sigma, 2) * (1 - N * exp(M * T)));
+        return exp(A + C * V + i * u * X);
     }
-
-    double theta(std::string type) {
-        double d1 = (log(pricer.S0 / pricer.K) +
-                     (pricer.r - pricer.q + 0.5 * pricer.sigma * pricer.sigma) * tau) /
-                    (pricer.sigma * sqrt(tau));
-        switch (type) {
-            case "call":
-                return -pricer.S0 * pricer.sigma * exp(-pricer.q * tau) * norm_pdf(d1) /
-                           (2 * sqrt(tau)) -
-                       pricer.r * pricer.K * exp(-pricer.r * tau) * norm_cdf(d2);
-            case "put":
-                return -pricer.S0 * pricer.sigma * exp(-pricer.q * tau) * norm_pdf(d1) /
-                           (2 * sqrt(tau)) +
-                       pricer.r * pricer.K * exp(-pricer.r * tau) * norm_cdf(-d2);
-            default:
-                throw std::invalid_argument("Invalid option type");
-        }
-    }
-
-    double vega() {
-        double d1 = (log(pricer.S0 / pricer.K) +
-                     (pricer.r - pricer.q + 0.5 * pricer.sigma * pricer.sigma) * tau) /
-                    (pricer.sigma * sqrt(tau));
-        return pricer.S0 * exp(-pricer.q * tau) * norm_pdf(d1) * sqrt(tau);
-    }
-
-    double rho(std::string type) {
-        double d2 = (log(pricer.S0 / pricer.K) +
-                     (pricer.r - pricer.q + 0.5 * pricer.sigma * pricer.sigma) * tau) /
-                    (pricer.sigma * sqrt(tau));
-        switch (type) {
-            case "call":
-                return pricer.K * tau * exp(-pricer.r * tau) * norm_cdf(d2);
-            case "put":
-                return -pricer.K * tau * exp(-pricer.r * tau) * norm_cdf(-d2);
-            default:
-                throw std::invalid_argument("Invalid option type");
-        }
-    }
-    PricerBS get_pricer(double S, double K, double tau, double implied_volatility) {
-        PricerBS new_pricer = pricer;
-        new_pricer.S0 = S;
-        new_pricer.K = K;
-        new_pricer.T = tau; 
-        new_pricer.sigma = implied_volatility;
-        return new_pricer;
-    }
-    Greeks get_greeks(double S, double K, double tau, double implied_volatility) {
-        PricerBS new_pricer = get_pricer(S, K, tau, implied_volatility);
-        return Greeks(new_pricer, tau);
-    }
-    
-};
-
-class ConstructPortfolio {
-    PricerBS pricer;
-    double K1;
-    vector<vector<double>> stock_path;
-    double K2;
-    vector<string> option_types;
-    vector<double> implied_volatilities;
-
-    ConstructPortfolio(PricerBS pricer, double tau, double K1, double K2,
-                       vector<string> option_types, vector<double> implied_volatilities, vector<vector<double>> stock_path)
-        : pricer(pricer),
-          tau(tau),
-          K1(K1),
-          K2(K2),
-          option_types(option_types),
-          implied_volatilities(implied_volatilities),
-          stock_path(stock_path) {}
-
-    
 }
 
-
-
-
-
-
-
-
-
-int main() {
-    double S0 = 100;
-    double T = 1;
-    double sigma = 0.2;
-    double r = 0.05;
-    int N = 100;
-    int M = 1000;
-    vector<vector<double>> stock_path = GBM(S0, T, sigma, r, N, M);
-    cout << stock_path[0][0] << endl;
-    ofstream file("stock_path.csv");
-    for (int m = 0; m < M; m++) {
-        for (int i = 0; i <= N; i++) {
-            file << stock_path[m][i] << ",";
+    int main() {
+        double S0 = 100;
+        double drift = 0.05;
+        double T = 1;
+        double v0 = 0.09;
+        double kappa = 2;
+        double theta = 0.04;
+        double sigma = 0.5;
+        double rho = -0.75;
+        int N = 100;
+        int M = 10000;
+        vector<vector<double>> stock_path =
+            HestonSimultion(S0, drift, T, v0, kappa, theta, sigma, rho, N, M);
+        ofstream file("stock_path.csv");
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j <= N; j++) {
+                file << stock_path[i][j] << ",";
+            }
+            file << endl;
         }
-        file << endl;
+        file.close();
+        return 0;
     }
-    file.close();
-    return 0;
-}
